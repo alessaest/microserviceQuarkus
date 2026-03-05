6613ad00b72e0bf17c69792aeaaa5c8d1c663918
Alessa <alessuhestaras@gmail.com>
2026-03-05 17:08:56 +0800

    Executing and Invoking REST endpoint for Book Microservice

--- vintageStore/rest-book/src/main/java/org/agoncal/quarkus/microservices/BookResource.java
+++ vintageStore/rest-book/src/main/java/org/agoncal/quarkus/microservices/BookResource.java
@@ -1,16 +1,32 @@
 package org.agoncal.quarkus.microservices;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
+
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Inject
+    Logger logger;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createABook(@FormParam("title") String title, @FormParam("author") String author, @FormParam("year") int yearOfPublication, @FormParam("genre") String genre) {
+
+        Book book = new Book();
+        book.isbn13 = "It will be generated later by the number microservice";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        logger.info("Book created: " + book);
+        return Response.status(201).entity(book).build();
     }
 }
