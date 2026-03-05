783668bb3bf540605713528ad602668cb5939879
Alessa <alessuhestaras@gmail.com>
2026-03-05 16:30:32 +0800

    Tested the number resource

--- vintageStore/rest-number/src/test/java/org/agoncal/quarkus/microservices/NumberResourceTest.java
+++ vintageStore/rest-number/src/test/java/org/agoncal/quarkus/microservices/NumberResourceTest.java
@@ -5,16 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
 class NumberResourceTest {
-    @Test
-    void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello RESTEasy"));
-    }
 
+    @Test
+    public void shouldGenerateIsbnNumbers(){
+        given().
+        when().get("/api/numbers").
+        then()
+                .statusCode(200)
+                .body("isbn_13", startsWith("13-"))
+                .body("isbn_10", startsWith("10-"))
+                .body(not(hasKey("generationDate")));
+    }
 }
